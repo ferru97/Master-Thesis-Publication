f0a835db6191d396c3d79dba440d14f9619eda0f
damiano <damiano@gridnode4.iit.cnr.it>
2023-04-23 13:08:09 +0200

    update

--- docker_containers/remote/scripts/create_jobs.sh
+++ docker_containers/remote/scripts/create_jobs.sh
@@ -3,7 +3,7 @@
 #docker exec -it chainlink_node11 bash -c 'cd /home/root; chmod +x chainlink_login.sh; chmod +x chainlink_delete_job.sh; ./chainlink_login.sh; ./chainlink_delete_job.sh; chainlink jobs create "/home/root/job.toml";exit'
 #echo "---------------------------------------"
 
-count=2
+count=1
 for i in $(seq $count); do
 	echo "Creating job on Oracle $i"
     node="chainlink_node$i"
